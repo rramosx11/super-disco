6c3ef4e72cc1c0392ef098e5e5baca98799c8e1c
rramosx11 <rodolforamosd11@gmail.com>
2022-07-07 16:21:43 -0400

    storage and color set up

--- README.md
+++ README.md
@@ -21,3 +21,5 @@ WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
+
+## Deployed URL/Screenshot
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// var dateToday = document.querySelector("#date-today");
+// var dateToday = document.querySelector("#currentDay");
 // var currentTime = moment();
 
 // dateToday.textContent =
@@ -6,6 +6,48 @@
 //   "," +
 //   currentTime.format("MMM DD, YYYY - hh:mm:ss a");
 
-$("#date-today h6").text(
-  moment().format("dddd") + ", " + moment().format("MMMM Do YYYY, h:mm:ss a")
-);
+// $("#currentDay").text(
+//   moment().format("dddd") + ", " + moment().format("MMMM Do YYYY, h:mm:ss a")
+// );
+
+$(document).ready(function () {
+  $("#currentDay").text(
+    moment().format("dddd") + ", " + moment().format("MMMM Do YYYY, h:mm:ss a")
+  );
+  $(".saveBtn").on("click", function () {
+    var plannerText = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, plannerText);
+  });
+
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
+
+  // add more rows for more time
+  function timeChange() {
+    var currentHour = moment().hour(); // Number
+
+    $(".time-block").each(function () {
+      var idTime = parseInt($(this).attr("id"));
+      if (idTime < currentHour) {
+        $(this).addClass("past");
+      } else if (idTime === currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("past");
+      } else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+    });
+  }
+  timeChange();
+  var timeInterval = setInterval(timeChange, 1000);
+});
