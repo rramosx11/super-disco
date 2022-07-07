0ed0ac4cad5dfced327c0ca011a87f54844c442e
rramosx11 <rodolforamosd11@gmail.com>
2022-07-06 23:29:26 -0400

    added current time to top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+// var dateToday = document.querySelector("#date-today");
+// var currentTime = moment();
 
+// dateToday.textContent =
+//   currentTime.format("dddd") +
+//   "," +
+//   currentTime.format("MMM DD, YYYY - hh:mm:ss a");
+
+$("#date-today h6").text(
+  moment().format("dddd") + ", " + moment().format("MMMM Do YYYY, h:mm:ss a")
+);
